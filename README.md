87147d5c0cb3097ed21475852aefa12285ed1539
viek4nag <vivek4nag@gmail.com>
2024-09-10 02:05:20 +0530

    done

---
+++ README.md
@@ -0,0 +1,18 @@
+# To-Do List Web App
+
+This is a simple To-Do List web application that allows users to add, check/uncheck, and delete tasks. The tasks are saved in the browser's local storage, so they remain persistent even after refreshing the page.
+
+[✍️TRY IT NOW✍️](https://vivek4nag.github.io/To-Do-List/)
+
+## Features
+
+- Add new tasks to the list.
+- Mark tasks as completed or uncompleted by clicking on the task.
+- Delete tasks by clicking the close (✕) button.
+- Tasks are saved in local storage, so the list persists even after a page reload.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript (DOM Manipulation & Local Storage)
--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// adding task to list
 function addTask(){
     if(inputBox.value === ''){
-        alert("please Enter a task");
+        alert("please Enter a task✍️✍️");
     }
     else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value
-        listContainer.appendChild(li);
+        let li = document.createElement("li"); // creating a list element
+        li.innerHTML = inputBox.value // getting the input text of user using innerHtml
+        listContainer.appendChild(li); // appending tht as list item
+
+        //adding a cross at end of list element
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
     }
+    inputBox.value =""; // clearing input field after every insertion
+    saveData() // we are calling saveddata every time after any task & will save updated content
 }
+
+// for click function
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked"); // if clicked on LI either it will be checked or unchecked
+        saveData()
+    }
+    else if(e.target.tagName === "SPAN"){ // span is cross wala icon. so if clicked on that it will delete parent element i.e. list
+        e.target.parentElement.remove();
+        saveData()
+    } // save data will be called for oth checking/unchekcing or deletion of data to update local storage
+}, false)
+
+//function to store data locally
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// function to display data whenever we refresh
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+
+showTask(); // simply calling funtion evrytime

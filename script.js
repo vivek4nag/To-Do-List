e813507ba66a032ea9496ed1b35200b7cbcc6f1e
viek4nag <vivek4nag@gmail.com>
2024-09-10 01:18:01 +0530

    JS updated

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("please Enter a task");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value
+        listContainer.appendChild(li);
+    }
+}
